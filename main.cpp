b23ef0315e33a06d1d5a2501c2b90f60c899f7e8
jacob1024 <liushanchao5@gmail.com>
2021-01-18 22:13:04 +0800

    项目的第一次提交.

---
+++ main.cpp
@@ -0,0 +1,34 @@
+/**********
+This library is free software; 
+you can redistribute it and/or modify it underthe terms of the 
+GNU Lesser General Public License as published by the Free Software Foundation; 
+either version 3 of the License, or (at youroption) any later version. 
+(See <http://www.gnu.org/copyleft/lesser.html>.)
+This library is distributed in the hope that it will be useful, 
+but WITHOUTANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESSFOR A PARTICULAR PURPOSE. 
+See the GNU Lesser General Public License formore details.
+You should have received a copy of the GNU Lesser General Public Licensealong with this library; 
+
+By LwDB Group
+**********/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+
+
+
+int main()
+{
+	printf("first commit \n");
+
+	return 0;
+}
+
+
+
+
